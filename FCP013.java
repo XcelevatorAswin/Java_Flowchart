1d76b78c3532d055b11631b85047b4c6d272cf4a
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-07-30 11:50:14 +0530

    FCP013

---
+++ FCP013.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP013{
+	public static void main(String [] args){
+	Scanner scan= new Scanner(System.in);
+	int a=scan.nextInt();
+	if(a%2==0){
+	System.out.println(a);}
+	else{
+	a=a*2;
+	System.out.println(a);}
+}
+}
