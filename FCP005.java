9a5cd26d7ee53d3a560073c53478c5040f4daac8
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-07-30 09:58:39 +0530

    FCP005

---
+++ FCP005.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FCP005{
+	public static void main(String [] args){
+Scanner scan=new Scanner(System.in);
+	int a=scan.nextInt();
+	int b=scan.nextInt();
+	int d;
+	int e;
+	d=a/b;
+	e=a%b;
+	System.out.println(d);
+	System.out.println(e);
+}
+}
