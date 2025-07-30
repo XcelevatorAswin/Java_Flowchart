f2112bc79e6423fc677291b97608badebdd93f19
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-07-30 10:18:33 +0530

    FCP007

---
+++ FCP007.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP007{
+	public static void main(String [] args){
+	Scanner scan=new Scanner(System.in);
+	int c=scan.nextInt();
+	int f;
+	f=(c*9/5)+32;
+	System.out.println(f);
+}
+}
