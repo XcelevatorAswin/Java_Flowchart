8add351b28b009f25be2ccb150e51c4f16b76bde
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-07-30 11:36:34 +0530

    FCP011

---
+++ FCP011.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FCP011{
+	public static void main(String [] args){
+	Scanner scan=new Scanner(System.in);
+	int a=scan.nextInt();
+	if(a%2==0){
+	System.out.println("even");}
+	else{
+	System.out.println("Odd");}
+}
+}
