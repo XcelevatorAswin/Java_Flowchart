dbdeb4d1f075d19b4e1ec5e65d06b27749ad7ffc
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-07-30 10:05:54 +0530

    FCP006

---
+++ FCP006.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP006{
+	public static void main(String [] args){
+	Scanner scan=new Scanner(System.in);
+	int a=scan.nextInt();
+	int c;
+	c=a%10;
+	System.out.println(c);
+}
+}
