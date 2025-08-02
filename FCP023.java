08230c8f0b57bb63a3064d809da02e43199c5417
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-08-02 15:15:16 +0530

    FCP023

---
+++ FCP023.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP023{
+public static void main(String [] args){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+String word = scan.next();
+for(int i=0;i<n;i++){
+System.out.println(word);}
+}
+}
