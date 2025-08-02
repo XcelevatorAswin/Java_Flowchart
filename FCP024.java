d97acae8a96fec4f8f5d4ea6d61884b26992c443
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-08-02 15:40:56 +0530

    FCP024

---
+++ FCP024.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP024{
+public static void main(String [] args){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int i=0;
+for(i=0;i<n;i+=2){
+System.out.println(i);}
+}
+}
